54464655bb02dd34e3daba8e344a894d3debd997
Mon Doggner <archiesupergood@gmail.com>
2024-01-10 16:28:22 +0300

    Замена списков для кодирование на генераторы

--- Zippy.py
+++ Zippy.py
@@ -17,16 +17,16 @@ sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
 sidebar_frame.grid_rowconfigure(4, weight=1)
 
 zippy_logo = customtkinter.CTkLabel(
-sidebar_frame, 
-text='Zippy', 
-font=('system', 40)
+    sidebar_frame, 
+    text='Zippy', 
+    font=('system', 40)
 )
 zippy_logo.grid(row=0, column=0, padx=20, pady=(20, 10))
 
 counter_label = customtkinter.CTkLabel(
-app, 
-text='Подсчёт символов', 
-font=('system', 22)
+    app, 
+    text='Подсчёт символов', 
+    font=('system', 22)
 )
 counter_label.grid(row=0, column=1, padx=30, pady=20, sticky='nw')
 
@@ -34,9 +34,9 @@ counter_textbox = customtkinter.CTkTextbox(app, width=600)
 counter_textbox.grid(row=0, column=1, padx=20, pady=55, sticky='nw')
 
 code_label = customtkinter.CTkLabel(
-app, 
-text='Кодирование', 
-font=('system', 22)
+    app, 
+    text='Кодирование', 
+    font=('system', 22)
 )
 code_label.grid(row=1, column=1, padx=30, pady=0, sticky='nw')
 
@@ -48,10 +48,7 @@ def open_file_func():
     if filepath != "":
         with open(filepath, "r", encoding= "UTF-8") as file: 
             data = file.read()
-            counter = {}
-            temp_keys = []
-            temp_values = []
-            
+            counter = {}            
 
     for i in data:
         if i in counter:
@@ -61,21 +58,21 @@ def open_file_func():
 
     for i in counter:
         counter_textbox.insert(END, f''''{i}' - {counter[i]}''' + '\n')
-        temp_keys.append(i)
-        temp_values.append(counter[i])
+        temp_keys = [i for i in counter]
+        temp_values = [counter[i] for i in counter]
 
     print(temp_keys)
     print(temp_values)
 
 open_file_button = customtkinter.CTkButton(
-master=app,
-width=100,
-height=25,
-border_width=0,
-corner_radius=8,
-text='Open file',
-font=('system', 20),
-command=open_file_func
+    master=app,
+    width=100,
+    height=25,
+    border_width=0,
+    corner_radius=8,
+    text='Open file',
+    font=('system', 20),
+    command=open_file_func
 )    
 open_file_button.place(relx=0.025, rely=0.15, anchor=NW)    
 
