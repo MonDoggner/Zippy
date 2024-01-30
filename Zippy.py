b6c695fe6d48beb48ac0c0892544d68497a45bb0
Mon Doggner <archiesupergood@gmail.com>
2024-01-30 17:21:01 +0300

    Начало ооп

--- Zippy.py
+++ Zippy.py
@@ -51,41 +51,49 @@ class App(customtkinter.CTk):
         )
         self.app_logo.grid(row=0, column=0, padx=20, pady=(20, 10))
 
-app = App(
-    app_name='Zippy', 
-    app_icon='Zippy icon.ico', 
-    app_size='900x700', 
-    app_resizable=False
-)
+
 
 class FirstScreen(App):
     def __init__(self, app_name: str, app_icon: str, app_size: str, app_resizable: bool):
         super().__init__(app_name, app_icon, app_size, app_resizable)
 
         self.counter_label = customtkinter.CTkLabel(
-            app, 
+            self, 
             text='Подсчёт символов', 
             font=('system', 22)
         )
         self.counter_label.grid(row=0, column=1, padx=30, pady=20, sticky='nw')
 
-        self.counter_textbox = customtkinter.CTkTextbox(app, width=600)    
+        self.counter_textbox = customtkinter.CTkTextbox(self, width=600)    
         self.counter_textbox.grid(row=0, column=1, padx=20, pady=55, sticky='nw')
 
         self.code_label = customtkinter.CTkLabel(
-            app, 
+            self, 
             text='Кодирование', 
             font=('system', 22)
         )
         self.code_label.grid(row=1, column=1, padx=30, pady=0, sticky='nw')
 
-        self.code_textbox = customtkinter.CTkTextbox(app, width=600)    
+        self.code_textbox = customtkinter.CTkTextbox(self, width=600)    
         self.code_textbox.grid(row=1, column=1, padx=20, pady=35, sticky='nw')
 
+        self.open_file_button = customtkinter.CTkButton(
+            master=self,
+            width=100,
+            height=25,
+            border_width=0,
+            corner_radius=8,
+            text='Open file',
+            font=('system', 20),
+            command=FirstScreen.open_file
+        )    
+        self.open_file_button.place(relx=0.025, rely=0.15, anchor=NW)
+
+    @classmethod
     def open_file(self):
-        filepath = filedialog.askopenfilename()
-        if filepath != "":
-            with open(filepath, "r", encoding= "UTF-8") as file: 
+        self.filepath = filedialog.askopenfilename()
+        if self.filepath != "":
+            with open(self.filepath, "r", encoding= "UTF-8") as file: 
                 global data
                 global counter            
                 try:
@@ -94,28 +102,31 @@ class FirstScreen(App):
                     self.char_counter(data)
                     self.counter_textbox_print() 
                     self.code_textbox_print()
+                    with self.open('logs.txt', 'a', encoding='UTF-8') as logs:
+                        logs.write(f'{datetime.now()}\nУспешное кодирование {self.filepath}\n\n')
+                except UnicodeDecodeError:
                     with open('logs.txt', 'a', encoding='UTF-8') as logs:
-                        logs.write(f'{datetime.now()}\nУспешное кодирование {filepath}\n\n')
-                except Exception as e:
-                    with open('logs.txt', 'a', encoding='UTF-8') as logs:
-                        logs.write(f'{datetime.now()}\nОшибка:{e}\n\n')
-
-    def char_counter(char_data):
+                        logs.write(f'{datetime.now()}\nОшибка: Выбран не текстовый файл\n\n')
+    @classmethod
+    def char_counter(self, char_data):
         for i in char_data:
             if i in counter:
                 counter[i] += 1
             else:
                 counter[i] = 1
 
+    @classmethod
     def counter_textbox_print(self):
         for i in counter:
             self.counter_textbox.insert(END, f''''{i}' - {counter[i]}''' + '\n')
 
+    @classmethod
     def code_textbox_print(self):
         code = self.huffman_encode(data)
         for ch in code:
             self.code_textbox.insert(END, f''''{ch}' - {code[ch]}''' + '\n')    
 
+    @classmethod
     def huffman_encode(data):
         line = []
 
@@ -139,11 +150,19 @@ class FirstScreen(App):
         
         return code 
     
+    @classmethod
     def destroyer(self):
         self.counter_label.destroy()
         self.counter_textbox.destroy()
         self.code_label.destroy()
         self.code_textbox.destroy()
+
+app = FirstScreen(
+    app_name='Zippy', 
+    app_icon='Zippy icon.ico', 
+    app_size='900x700', 
+    app_resizable=False
+)
 
 def logs_textbox_print(self, data):
     self.logs_textbox.insert(END, f''''{data}''' + '\n')
@@ -162,19 +181,7 @@ def logs_open(self):
     logs_textbox = customtkinter.CTkTextbox(app, width=600)    
     logs_textbox.grid(row=1, column=1, padx=100, pady=35, sticky='nws')
 
-open_file_button = FirstScreen(
-    master=app,
-    width=100,
-    height=25,
-    border_width=0,
-    corner_radius=8,
-    text='Open file',
-    font=('system', 20),
-    command=open_file
-)    
-open_file_button.place(relx=0.025, rely=0.15, anchor=NW)
-
-logs_button = FirstScreen(
+logs_button = customtkinter.CTkButton(
     master=app,
     width=100,
     height=25,
